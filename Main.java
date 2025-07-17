4806c45f4e52f472cf7e68dda3aa166e7b3ec739
anujmaurya6 <maurya913639@gmail.com>
2025-07-17 21:34:28 +0530

    Practicing basic DSA

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
+
 public class Main{
-    public static void main(String[] args) {
-        String str="Anuj Maurya";
-        String reversed="";
-        for(int i=str.length()-1;i>=0;i--){
-            reversed=reversed+str.charAt(i);
-        }
-        System.out.println("the reverse of the String is:"+reversed);
-    }
+public static void main(String[] args) {
+String str="Anuj";
+int i=0;
+int j=3;
+
+StringBuilder sb=new StringBuilder(str);
+
+//swap karna
+char temp=sb.charAt(i);
+sb.setCharAt(i,sb.charAt(j));
+sb.setCharAt(i, temp);
+
+System.out.println("after swap"+sb.toString());
 }
+}
+    
